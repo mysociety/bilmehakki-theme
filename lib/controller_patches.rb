df23f99db556583a870a601bd25fe8e51581b0ba
Louise Crow <louise.crow@gmail.com>
2016-01-25 16:46:07 +0000

    Add Turkish alphabet.

--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -1,9 +1,20 @@
+# -*- encoding : utf-8 -*-
 # Add a callback - to be executed before each request in development,
 # and at startup in production - to patch existing app classes.
 # Doing so in init/environment.rb wouldn't work in development, since
 # classes are reloaded, but initialization is not run each time.
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
+
 Rails.configuration.to_prepare do
 
+  PublicBodyController.class_eval do
+
+    before_filter :get_turkish_alphabet, :only => [:list]
+
+    def get_turkish_alphabet
+      @turkish_alphabet = ["A","B","C","Ç","D","E","Ğ","F","G","H","İ","I","J","K","L","M","N","Ö","O","P","R","S","Ş","Ü","T","U","V","Y","Z"]
+    end
+
+  end
 end
