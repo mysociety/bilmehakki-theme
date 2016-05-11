4dda89778492b24491e6bf9421cd52e8e588334e
Gareth Rees <gareth@garethrees.co.uk>
2016-05-11 15:41:48 +0100

    Add UserAddressController, views and routes

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -2,6 +2,14 @@
 # Here you can override or add to the pages in the core website
 
 Rails.application.routes.draw do
+  match '/profile/address/edit' => 'user_address#edit',
+        :as => :user_edit_address,
+        :via => :get
+
+  match '/profile/address' => 'user_address#update',
+        :as => :user_update_address,
+        :via => :put
+
   match '/profile/identity_card_number/edit' => 'user_identity_card_number#edit',
         :as => :user_edit_identity_card_number,
         :via => :get
